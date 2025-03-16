15820611e0fdb966ac7091eeaa9ce3c33cebad3c
iamqazi <qaziobaid7897@gmail.com>
2025-03-16 23:33:32 +0500

    changes

--- app/listing/page.tsx
+++ app/listing/page.tsx
@@ -1,16 +1,23 @@
-import NewListings from '../components/listing-section'
-import NavBar from '../components/shared-ui/navbar'
+import RemorphCard from "../components/builder-section";
+import NewListings from "../components/listing-section";
+import Footer from "../components/shared-ui/footer";
+import NavBar from "../components/shared-ui/navbar";
+import { TokenTable } from "../components/trending-section";
+import { tokenData } from "../libs/data";
 
 export default function ListingPage() {
   return (
-    <div className='bg-[#030128] min-[1440px] relative'>
+    <div className="bg-[#030128] min-[1440px] relative">
       {/* Fixed Navbar with gap below */}
-      <div className='absolute top-[40px] flex justify-center items-center mx-auto left-0 right-0 z-20 '>
+      <div className="absolute top-[40px] flex justify-center items-center mx-auto left-0 right-0 z-20 ">
         <NavBar />
       </div>
-      <div className='pt-20'>
+      <div className="pt-20">
         <NewListings />
+        <RemorphCard />
       </div>
+      <TokenTable data={tokenData} itemsPerPage={10} />
+      <Footer />
     </div>
-  )
+  );
 }
