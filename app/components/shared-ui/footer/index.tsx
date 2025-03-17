e7d265394a3986745b5ddd381aec3c9f66386ae4
iamqazi <qaziobaid7897@gmail.com>
2025-03-18 01:33:22 +0500

    lint issues

--- app/components/shared-ui/footer/index.tsx
+++ app/components/shared-ui/footer/index.tsx
@@ -1,65 +1,62 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { useRouter } from 'next/navigation'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
-import React from 'react'
+import React from "react";
 
 const Footer: React.FC = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
-  const handleButtonClickHome = () => {
-    window.location.href = '/'
-  }
-  const router = useRouter()
+  const router = useRouter();
 
   const scrollToElement = (id: string) => {
     const checkExist = setInterval(() => {
-      const element = document.getElementById(id)
+      const element = document.getElementById(id);
       if (element) {
-        element.scrollIntoView({ behavior: 'smooth' })
-        clearInterval(checkExist)
+        element.scrollIntoView({ behavior: "smooth" });
+        clearInterval(checkExist);
       }
-    }, 100)
-  }
+    }, 100);
+  };
   const handleAboutClick = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault()
-    if (pathname !== '/') {
-      router.push('/')
+    e.preventDefault();
+    if (pathname !== "/") {
+      router.push("/");
       setTimeout(() => {
-        scrollToElement('about')
-      }, 1000)
+        scrollToElement("about");
+      }, 1000);
     } else {
-      scrollToElement('about')
+      scrollToElement("about");
     }
-  }
+  };
   return (
-    <div className='bg-[#030128] text-white flex flex-col md:px-0 px-[16px] '>
+    <div className="bg-[#030128] text-white flex flex-col md:px-0 px-[16px] ">
       {/* Main content area */}
 
       {/* Footer Card */}
-      <div className='mx-auto w-full max-w-[1226px] mb-4'>
+      <div className="mx-auto w-full max-w-[1226px] mb-4">
         <div
           style={{
-            boxShadow: '0px 0px 55px 0px #C5B9F626 inset',
+            boxShadow: "0px 0px 55px 0px #C5B9F626 inset",
           }}
-          className='bg-[#0E0D2D] border border-[#C5B9F633] rounded-2xl p-[30px] md:p-[70px] shadow-lg'
+          className="bg-[#0E0D2D] border border-[#C5B9F633] rounded-2xl p-[30px] md:p-[70px] shadow-lg"
         >
-          <div className='flex flex-col lg:flex-row justify-between gap-[20px]'>
+          <div className="flex flex-col lg:flex-row justify-between gap-[20px]">
             {/* Left Section */}
-            <div className='md:w-1/2 mb-8 md:mb-0'>
-              <div className='flex items-center mb-6'>
+            <div className="md:w-1/2 mb-8 md:mb-0">
+              <div className="flex items-center mb-6">
                 <Image
                   height={72}
                   width={245}
-                  src='/logo.png'
-                  alt='ReMorph Ai Logo'
-                  className=' mr-3'
+                  src="/logo.png"
+                  alt="ReMorph Ai Logo"
+                  className=" mr-3"
                 />
               </div>
 
-              <p className='text-[#DEDDFF] text-[18px] font-[300] max-w-[385px] mb-6'>
+              <p className="text-[#DEDDFF] text-[18px] font-[300] max-w-[385px] mb-6">
                 ReMorph AI — Reshaping Trust in Web3.
                 <br />
                 Building a safer and more transparent blockchain ecosystem with
@@ -69,45 +66,45 @@ const Footer: React.FC = () => {
             </div>
 
             {/* Right Section */}
-            <div className='w-2/5 flex flex-col '>
-              <div className=' '>
+            <div className="w-2/5 flex flex-col ">
+              <div className=" ">
                 <button
-                  onClick={() => (window.location.href = '/application')}
+                  onClick={() => (window.location.href = "/application")}
                   style={{
                     background:
-                      'linear-gradient(0deg, #7E62FF 34.55%, #C1B3FF 144.55%)',
+                      "linear-gradient(0deg, #7E62FF 34.55%, #C1B3FF 144.55%)",
                   }}
-                  type='submit'
+                  type="submit"
                   className={`cursor-pointer px-6 py-4 whitespace-nowrap md:text-[20px] h-[57px] text-[14px] md:w-[207px] border justify-center flex items-center gap-2 border-[#B6A6FF] text-white font-medium rounded-[12px]`}
                 >
                   Apply Now
                   <Image
-                    src={'/arrow.png'}
+                    src={"/arrow.png"}
                     height={20}
                     width={25}
-                    alt='arrow'
+                    alt="arrow"
                   />
                 </button>
               </div>
-              <div className='flex gap-[80px] max-w-[500px] mt-[30px]'>
+              <div className="flex gap-[80px] max-w-[500px] mt-[30px]">
                 {/* Docs Column */}
-                <div className='mb-6 md:mb-0 '>
-                  <h3 className='text-[16px] text-white font-[600] mb-4'>
+                <div className="mb-6 md:mb-0 ">
+                  <h3 className="text-[16px] text-white font-[600] mb-4">
                     Docs
                   </h3>
                   <ul>
-                    <li className='mb-2'>
+                    <li className="mb-2">
                       <a
-                        href='/listing'
-                        className='text-[#9B9BBD] text-[14px] hover:text-white'
+                        href="/listing"
+                        className="text-[#9B9BBD] text-[14px] hover:text-white"
                       >
                         Whitepaper
                       </a>
                     </li>
-                    <li className='mb-2'>
+                    <li className="mb-2">
                       <a
-                        href='#roadmap'
-                        className='text-[#9B9BBD] text-[14px] hover:text-white'
+                        href="#roadmap"
+                        className="text-[#9B9BBD] text-[14px] hover:text-white"
                       >
                         Roadmap
                       </a>
@@ -115,7 +112,7 @@ const Footer: React.FC = () => {
                     <li>
                       <a
                         onClick={handleAboutClick}
-                        className='text-[#9B9BBD] text-[14px] hover:text-white'
+                        className="text-[#9B9BBD] text-[14px] hover:text-white"
                       >
                         About Us
                       </a>
@@ -125,38 +122,38 @@ const Footer: React.FC = () => {
 
                 {/* Contact Column */}
                 <div>
-                  <h3 className='text-[16px] text-white font-[600] mb-4'>
+                  <h3 className="text-[16px] text-white font-[600] mb-4">
                     Contact
                   </h3>
                   <ul>
-                    <li className='mb-2'>
+                    <li className="mb-2">
                       <a
-                        href='https://discord.gg/ZBE8DkJac7'
-                        className='text-[#9B9BBD] text-[14px] hover:text-white'
+                        href="https://discord.gg/ZBE8DkJac7"
+                        className="text-[#9B9BBD] text-[14px] hover:text-white"
                       >
                         Discord
                       </a>
                     </li>
-                    <li className='mb-2'>
+                    <li className="mb-2">
                       <a
-                        href='#'
-                        className='text-[#9B9BBD] text-[14px] hover:text-white'
+                        href="#"
+                        className="text-[#9B9BBD] text-[14px] hover:text-white"
                       >
                         Email
                       </a>
                     </li>
-                    <li className='mb-2'>
+                    <li className="mb-2">
                       <a
-                        href='https://t.me/ReMorphAI'
-                        className='text-[#9B9BBD] text-[14px] hover:text-white'
+                        href="https://t.me/ReMorphAI"
+                        className="text-[#9B9BBD] text-[14px] hover:text-white"
                       >
                         Telegram
                       </a>
                     </li>
                     <li>
                       <a
-                        href='https://x.com/RemorphAi'
-                        className='text-[#9B9BBD] text-[14px] hover:text-white'
+                        href="https://x.com/RemorphAi"
+                        className="text-[#9B9BBD] text-[14px] hover:text-white"
                       >
                         Twitter
                       </a>
@@ -172,20 +169,22 @@ const Footer: React.FC = () => {
       </div>
 
       {/* Copyright Bar */}
-      <div className=' text-[14px] text-[#777599] py-4'>
-        <div className='max-w-[1226px] font-sans mx-auto flex flex-col md:flex-row justify-between items-center'>
-          <div className=' font-sans'>
+      <div className=" text-[14px] text-[#777599] py-4">
+        <div className="max-w-[1226px] font-sans mx-auto flex flex-col md:flex-row justify-between items-center">
+          <div className=" font-sans">
             ©️ 2025 ReMorph AI. All rights reserved.
           </div>
           <div>
-            <Link href={'/privacy-policy'} className=' font-sans'>Privacy - Policy</Link>
-            <span className=' font-sans mx-2'>|</span>
-            <span className=' font-sans'>Powered by Digitaltodesign</span>
+            <Link href={"/privacy-policy"} className=" font-sans">
+              Privacy - Policy
+            </Link>
+            <span className=" font-sans mx-2">|</span>
+            <span className=" font-sans">Powered by Digitaltodesign</span>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
